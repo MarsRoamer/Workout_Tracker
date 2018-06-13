aec019c0cda472d71a83fac594d15c2cb50f92cb
Jonathan Tiede <jon@Jonathans-MacBook-Pro.local>
2018-06-13 17:30:35 -0600

    Gym owners can now create gyms

--- app/controllers/gym_controller.rb
+++ app/controllers/gym_controller.rb
@@ -5,16 +5,31 @@ class GymController < ApplicationController
 	end
 
 	def new
-
+		if owner_logged_in?
+			@owner = Owner.find_by(id: session[:owner_id])
+			@gym = Gym.new
+		else
+			redirect_to new_ownersession_path
+		end
 	end
 
 	def create
+		gym = current_owner.gyms.build(gym_params)
+		gym.save
+		redirect_to gym_path(gym)
+		
 
 	end
 
 	def show
-
+		@gym = Gym.find_by(id: params[:id])
 	end
+
+	private
+
+		def gym_params
+			params.require(:gym).permit(:name, :city, :state, :motto)
+		end
 
 
 	
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
 
 	root 'application#home'
 
-	resources :owners, only: [:index, :new, :create, :edit, :update, :show]
+	resources :owners, only: [:index, :new, :create, :edit, :update, :show] do 
+		resources :gym, only: [:new, :edit, :update, :create]
+	end
 	resources :ownersessions, only: [:new, :create, :destroy]
 	resources :sessions, only: [:new, :create, :destroy]
-	resources :gym, only: [:index, :new, :create, :update, :show]
+	resources :gym, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
